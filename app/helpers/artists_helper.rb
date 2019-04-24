ad36c4e7f45d67ab1acb1174467dd401deca91da
Jonathan Luk <jluk87@gmail.com>
2019-04-24 18:08:47 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,7 +2,7 @@ module ArtistsHelper
 
   def display_artist(song)
     if song.artist
-      link_to song.artist.name, artist_path(song.artist)
+      link_to song.artist_name, artist_path(song.artist)
     else
       link_to 'Edit Song', edit_song(song)
     end
