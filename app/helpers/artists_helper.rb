f979749cc115f3810380fa12867f449342a7891c
Jonathan Luk <jluk87@gmail.com>
2019-04-24 18:43:47 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,7 +1,7 @@
 module ArtistsHelper
 
   def display_artist(song)
-    unless song.artist_name.blank?
+    if !song.artist_name.blank?
       link_to song.artist_name, artist_path(song.artist)
     else
       link_to 'Add Artist', edit_song_path(song)
